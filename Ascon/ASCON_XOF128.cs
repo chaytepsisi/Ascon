cc431dfc75712b56e2feb99702293dbad8946c1b
chaytepsisi <34841956+chaytepsisi@users.noreply.github.com>
2025-04-07 14:02:55 +0300

    Add files via upload
    Hash256 and XOF128 files are added manually.

---
+++ Ascon/ASCON_XOF128.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ascon
+{
+    public class ASCON_XOF128
+    {
+        public ulong[] GenerateOutput(ulong[] message, int messageLength, int outputLength)
+        {
+            ulong[] state = new ulong[5];//320bit state
+            ulong[] zeroKey = new ulong[2] { 0x0, 0x0 };
+            ulong[] zeroNonce = new ulong[2] { 0x0, 0x0 };
+
+            message = AsconOperations.Pad(message, messageLength);
+            AsconOperations.Initialization(state, zeroKey, AsconConstants.XOF_IV, zeroNonce);
+            AsconOperations.AbsorbMessage(state, message);
+
+            int outputBlockSize = (int)Math.Ceiling(outputLength / 64.0);
+            ulong[] xofOutput = new ulong[outputBlockSize];
+            xofOutput[0] = state[0];
+            for (int i = 1; i < outputBlockSize; i++)// 256/64 = 4 output blocks for 256bit hash
+            {
+                AsconOperations.Permutation(state, 0, AsconConstants.NumberOfRounds_Pa);
+                xofOutput[i] = state[0];
+            }
+            return xofOutput;
+        }
+    }
+}
---
+++ Ascon/Ascon_Hash256.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ascon
+{
+    public class Ascon_Hash256
+    {
+        public ulong[] GenerateAsconHash(ulong[] message, int messageLength)
+        {
+            ulong[] state = new ulong[5];//320bit state
+            ulong[] zeroKey = new ulong[2] { 0x0, 0x0 };
+            ulong[] zeroNonce = new ulong[2] { 0x0, 0x0 };
+
+            message = AsconOperations.Pad(message, messageLength);
+            AsconOperations.Initialization(state, zeroKey, AsconConstants.HASH_IV, zeroNonce);
+            AsconOperations.AbsorbMessage(state, message);
+
+            ulong[] hashValue = new ulong[4];
+            hashValue[0] = state[0];
+            for (int i = 1; i < 4; i++)// 256/64 = 4 output blocks for 256bit hash
+            {
+                AsconOperations.Permutation(state, 0, AsconConstants.NumberOfRounds_Pa);
+                hashValue[i] = state[0];
+            }
+            return hashValue;
+        }
+    }
+}
